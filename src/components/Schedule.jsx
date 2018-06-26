742103ee06ffaae59b0b27b0d56b5acf2b6b1971
Student <example@example.com>
2018-06-26 16:08:43 -0700

    add loop for market schedule data

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,48 +1,32 @@
 import React from "react";
+import marketSchedule from './ScheduleData';
+import ScheduleItem from './ScheduleItem';
 
-function Schedule (){
+function Schedule(){
 
-  const marketSchedule = [
- {
-    day: "Sunday",
-    location: "Lents International",
-    hours: "9:00am - 2:00pm",
-    booth: "4A"
- },
- {
-    day: "Monday",
-    location: "Pioneer Courthouse Square",
-    hours: "10:00am - 2:00pm",
-    booth: "7C"
- },
- {
-    day: "Tuesday",
-    location: "Hillsboro",
-    hours: "5:00pm - 8:30pm",
-    booth: "1F"
- },
- {
-    day: "Wednesday",
-    location: "Shemanski Park",
-    hours: "10:00am - 2:00pm",
-    booth: "3E"
- },
- {
-    day: "Thursday",
-    location: "Northwest Portland",
-    hours: "2:00pm - 6:00pm",
-    booth: "6D"
- },
- {
-    day: "Saturday",
-    location: "Beaverton",
-    hours: "10:00am - 1:30pm",
-    booth: "9G"
- }
-];
 
   return (
-    <p>Hiya</p>
+    <div>
+      <h2>Market Schedule</h2>
+      <div className="market-container">
+      {marketSchedule.map((scheduleItem, index) =>
+      <ScheduleItem day={scheduleItem.day}
+        location={scheduleItem.location}
+        hours={scheduleItem.hours}
+        booth={scheduleItem.booth}
+        key={index}/>
+      )}
+      </div>
+      <style jsx>{`
+          .market-container{
+            display: grid;
+            grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+            grid-auto-flow: dense;
+            max-width: 1000px;
+          }
+          `}</style>
+
+    </div>
   );
 }
 
--- src/components/ScheduleItem.jsx
+++ src/components/ScheduleItem.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function ScheduleItem () {
-  return ();
+function ScheduleItem (props) {
+  return (
+    <div className="scheduleItem">
+      <p><strong>Day: </strong>{props.day}</p>
+      <p><strong>Location: </strong>{props.location}</p>
+      <p><strong>Hours: </strong>{props.hours}</p>
+      <p><strong>Booth: </strong>{props.booth}</p>
+
+      <style jsx>{`
+        .scheduleItem{
+          font-size: 1.2rem;
+        }
+            `}
+
+      </style>
+
+
+    </div>
+  );
+}
+
+ScheduleItem.PropTypes = {
+  day: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  hours: PropTypes.string.isRequired,
+  booth: PropTypes.string.isRequired,
 }
 
 export default ScheduleItem;
